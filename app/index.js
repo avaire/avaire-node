6813e753ac5f54a74bf1b1ce4858d022357f4176
Alexis Tan <alexis@sen-dev.com>
2017-03-10 17:17:54 +0100

    Create command throttle functionality

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ module.exports = {
     // Utilities
     logger: require('./utils/logger/Logger'),
     cache: require('./utils/cache/CacheManager'),
+    throttle: require('./utils/throttle/Throttle'),
     scheduler: require('./utils/scheduler/Scheduler'),
     configLoader: require('./utils/config/ConfigLoader'),
 
---
+++ app/utils/throttle/Throttle.js
@@ -0,0 +1,41 @@
+class Throttle {
+
+    /**
+     * Checks if the fingerprint is allowed to run with the provided settings.
+     * 
+     * @param  {String}  fingerprint   The fingerprint that is used to store data in the cache
+     * @param  {Integer} maxAttempts   The max number of attempts before the method should return false
+     * @param  {Integer} decaySeconds  The number of seconds that has to pass for the cache to decay
+     * @return {Boolean}
+     */
+    can(fingerprint, maxAttempts, decaySeconds) {
+        let item = this.getThrottleItem(fingerprint);
+
+        if (item.data++ >= maxAttempts) {
+            return false;
+        }
+
+        return this.getCacheAdapter().put(fingerprint, item.data++, decaySeconds);
+    }
+
+    /**
+     * Gets the raw cache throttle item.
+     * 
+     * @param  {String} fingerprint  The fingerprint that is used to store data in the cache
+     * @return {Object}
+     */
+    getThrottleItem(fingerprint) {
+        return this.getCacheAdapter().getRaw(fingerprint, 0);
+    }
+
+    /**
+     * Returns the memory cache adapter.
+     * 
+     * @return {MemoryCache}
+     */
+    getCacheAdapter() {
+        return app.cache.resolveAdapter('memory');
+    }
+}
+
+module.exports = new Throttle;
