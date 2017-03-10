eac34dd01eb22e441867d05737f90b25b49c8e77
Alexis Tan <alexis@sen-dev.com>
2017-03-10 17:48:04 +0100

    Add a throttle channel and guild middleware

--- app/bot/index.js
+++ app/bot/index.js
@@ -5,6 +5,8 @@ module.exports = {
         'isBotAdmin': require('./middleware/IsBotAdmin'),
         'require': require('./middleware/Require'),
         'throttle.user': require('./middleware/ThrottleUser'),
+        'throttle.channel': require('./middleware/ThrottleChannel'),
+        'throttle.guild': require('./middleware/ThrottleGuild'),
     },
 
     // Event Handlers
