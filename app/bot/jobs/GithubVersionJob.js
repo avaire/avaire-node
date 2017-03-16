9f7b41e3a04f9d83ea9502cf6f9fdc4364dbe59f
Alexis Tan <alexis@sen-dev.com>
2017-03-16 15:58:02 +0100

    Add github version job
    This will pull the latest version from the public repository, this can be used in commands to check if people are using an older version of Ava, it can also be used for bug reports to make it easier to compare the issue to the version number.

---
+++ app/bot/jobs/GithubVersionJob.js
@@ -0,0 +1,69 @@
+/** @ignore */
+const request = require('request');
+/** @ignore */
+const Job = require('./Job');
+
+/**
+ * Github version job, this job runs once an hour, every cycle the
+ * job will fetch the latest version number of AvaIre from the
+ * public github repository and store them in the file cache.
+ *
+ * @extends {Job}
+ */
+class GithubVersionJob extends Job {
+
+    /**
+     * The jobs constructor, this will check if the cache
+     * already exists, if it doesn't it will create
+     * it by calling the run method.
+     */
+    constructor() {
+        super();
+
+        if (!app.cache.has('github.version')) {
+            this.run();
+        }
+    }
+
+    /**
+     * This method determines when the job should be execcuted.
+     *
+     * @override
+     * @param  {RecurrenceRule} rule  A node-schedule CRON recurrence rule instance
+     * @return {mixed}
+     */
+    runCondition(rule) {
+        return '30 * * * *';
+    }
+
+    /**
+     * The jobs main logic method, this method is executed
+     * whenever the {@link Job#runCondition} method returns true.
+     *
+     * @override
+     */
+    run() {
+        request({
+            headers: {'User-Agent': 'AvaIre-Discord-Bot'},
+            url: 'https://raw.githubusercontent.com/Senither/AvaIre/master/package.json',
+            method: 'GET'
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                try {
+                    let parsed = JSON.parse(body);
+
+                    if (typeof parsed.version === 'undefined') {
+                        return app.logger.error('Github Version Job: Attempted to fetch github version but json response returned an invliad package object');
+                    }
+
+                    app.cache.forever('github.version', parsed.version);
+                } catch (err) {
+                    app.logger.error('Github Version Job: The API returned an unconventional response.');
+                    app.logger.error(err);
+                }
+            }
+        });
+    }
+}
+
+module.exports = GithubVersionJob;
