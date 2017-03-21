8c24ecf6a5cdc71e7ec3dc16f00de539fb995580
Alexis Tan <alexis@sen-dev.com>
2017-03-21 19:14:16 +0100

    Add latest and remote versions to the version command
    The messages has also been reworked a little bit

--- app/bot/commands/system/VersionCommand.js
+++ app/bot/commands/system/VersionCommand.js
@@ -6,6 +6,7 @@ const Command = require('./../Command');
 class VersionCommand extends Command {
     constructor() {
         super('!', 'version', [], {
+            description: 'Displays the current version of the bot and how many versions behind it is (if any)',
             middleware: [
                 'throttle.channel:1,5'
             ]
@@ -16,45 +17,56 @@ class VersionCommand extends Command {
         let repositoryVersion = app.cache.get('github.version', app.version);
         let versionType = semverDiff(app.version, repositoryVersion);
         let versionMessage = [
-            'You are currently `:difference` :type versions behind!',
+            'I am are currently `:difference` :type versions behind!',
             '',
             ':message',
             '',
-            'You can get the latest version from github:',
-            'https://github.com/senither/AvaIre/'
+            'You can get the latest version of me on github at [Senither/AvaIre](https://github.com/senither/AvaIre/)'
         ].join('\n');
+
+        let placeholders = undefined;
+        let embededElement = {
+            title: 'v' + app.version,
+            description: versionMessage,
+            footer: {
+                text: 'The latest version of AvaIre is v' + repositoryVersion
+            }
+        };
 
         // If the version differance is null we can assume we're using the lastest version.
         if (versionType === null) {
-            return app.envoyer.sendSuccess(message, 'You\'re using the latest version of AvaIre!');
-        }
-
-        if (versionType === 'patch') {
-            return app.envoyer.sendInfo(message, versionMessage, {
+            embededElement.color = app.envoyer.colors.success;
+            embededElement.description = 'You\'re using the latest version of AvaIre!';
+            embededElement.footer = undefined;
+        } else if (versionType === 'patch') {
+            embededElement.color = app.envoyer.colors.info;
+            placeholders = {
                 difference: this.versionDiff(repositoryVersion, 2),
                 type: versionType,
                 message: 'Patch version updates are bug fixes, refractoring of existing code and very minor changes that ' +
                          'wont affect other things in the code base, it is recommended that you update on patch version ' +
                          'changes to keep up with the bug fixes and patches.'
-            });
-        }
-
-        if (versionType === 'minor') {
-            return app.envoyer.sendWarn(message, versionMessage, {
+            };
+        } else if (versionType === 'minor') {
+            embededElement.color = app.envoyer.colors.warn;
+            placeholders = {
                 difference: this.versionDiff(repositoryVersion, 1),
                 type: versionType,
                 message: 'Minor version updates are new addtions, features and reworks of the existing codebase, it is ' +
                          'recommended that you update on minor version changes to keep up with the new features.'
-            });
+            };
+        } else {
+            embededElement.color = app.envoyer.colors.error;
+            placeholders = {
+                difference: this.versionDiff(repositoryVersion, 0),
+                type: versionType,
+                message: 'Major version updates are total reworks of the bot and how it works or a large compilation of ' +
+                         'minor changes to the source code, it is highly recommended that you update on major version ' +
+                         'changes since older versions will not be supported for bug fixes or updates.'
+            };
         }
 
-        return app.envoyer.sendError(message, versionMessage, {
-            difference: this.versionDiff(repositoryVersion, 0),
-            type: versionType,
-            message: 'Major version updates are total reworks of the bot and how it works or a large compilation of ' +
-                     'minor changes to the source code, it is highly recommended that you update on major version ' +
-                     'changes since older versions will not be supported for bug fixes or updates.'
-        });
+        return app.envoyer.sendEmbededMessage(message, embededElement, placeholders);
     }
 
     versionDiff(repoVersion, index) {
