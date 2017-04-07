4c52a3bb34ddf9bbd2f77f6c5aa14504cefc59e0
Alexis Tan <alexis@sen-dev.com>
2017-04-07 20:06:29 +0200

    Add uptime command

---
+++ app/bot/commands/general/UptimeCommand.js
@@ -0,0 +1,46 @@
+/** @ignore */
+const Command = require('./../Command');
+
+class UptimeCommand extends Command {
+    constructor() {
+        super('!', 'uptime', [], {
+            description: 'Tells you information about the bot itself.',
+            middleware: [
+                'throttle.user:1,2'
+            ]
+        });
+    }
+
+    onCommand(sender, message, args) {
+        return app.envoyer.sendInfo(message, 'I have been up for ' + this.getProcessUptime());
+    }
+
+    getProcessUptime() {
+        let seconds = process.uptime() + 50;
+
+        let d = Math.floor(seconds / 86400);
+        let h = Math.floor((seconds % 86400) / 3600);
+        let m = Math.floor(((seconds % 86400) % 3600) / 60);
+        let s = Math.floor(((seconds % 86400) % 3600) % 60);
+
+        if (d > 0) {
+            return `${this.prepare(d, 'day')}, ${this.prepare(h, 'hour')}, ${this.prepare(m, 'minute')}, and ${this.prepare(s, 'second')}`;
+        }
+
+        if (h > 0) {
+            return `${this.prepare(h, 'hour')}, ${this.prepare(m, 'minute')}, and ${this.prepare(s, 'second')}`;
+        }
+
+        if (m > 0) {
+            return `${this.prepare(m, 'minute')}, and ${this.prepare(s, 'second')}`;
+        }
+
+        return this.prepare(s, 'second');
+    }
+
+    prepare(number, name) {
+        return `${number} ${name}` + (number === 1 ? '' : 's');
+    }
+}
+
+module.exports = UptimeCommand;
