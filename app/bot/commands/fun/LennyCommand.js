944c8ac446f7bc8aa87e981f612508ea4a920feb
Alexis Tan <alexis@sen-dev.com>
2017-04-17 15:59:54 +0200

    Add a few one-line fun commands

---
+++ app/bot/commands/fun/LennyCommand.js
@@ -0,0 +1,30 @@
+/** @ignore */
+const Command = require('./../Command');
+
+class LennyCommand extends Command {
+
+    /**
+     * Sets up the command by providing the prefix, command trigger, any
+     * aliases the command might have and additional options that
+     * might be usfull for the abstract command class.
+     */
+    constructor() {
+        super('>', 'lenny', [], {
+            description: '( ͡° ͜ʖ ͡°)'
+        });
+    }
+
+    /**
+     * Executes the given command.
+     *
+     * @param  {IUser}     sender   The Discordie user object that ran the command.
+     * @param  {IMessage}  message  The Discordie message object that triggered the command.
+     * @param  {Array}     args     The arguments that was parsed to the command.
+     * @return {mixed}
+     */
+    onCommand(sender, message, args) {
+        return message.channel.sendMessage('( ͡° ͜ʖ ͡°)');
+    }
+}
+
+module.exports = LennyCommand;
