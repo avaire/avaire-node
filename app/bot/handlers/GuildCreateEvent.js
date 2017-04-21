67837055d6d7c76909cd604720c1692fbe0bbffa
Alexis Tan <alexis@sen-dev.com>
2017-04-21 18:47:50 +0200

    Create activity messages when the bot joins or leaves a guild

--- app/bot/handlers/GuildCreateEvent.js
+++ app/bot/handlers/GuildCreateEvent.js
@@ -3,7 +3,47 @@ const EventHandler = require('./EventHandler');
 
 class GuildCreateEvent extends EventHandler {
     handle(socket) {
-        //
+        app.logger.info(`Joined guild with an ID of ${socket.guild.id} called: ${socket.guild.name}`);
+
+        let avaireCentral = bot.Guilds.find(guild => guild.id === '284083636368834561');
+
+        if (typeof avaireCentral === 'undefined') { // Wat
+            return;
+        }
+
+        let channel = avaireCentral.textChannels.find(channel => channel.name === 'bot-activity');
+        if (typeof channel === 'undefined') {
+            return;
+        }
+
+        let owner = bot.Users.get(socket.guild.owner_id);
+
+        return app.envoyer.sendEmbededMessage(channel, {
+            title: 'Join Server',
+            color: 0x16D940,
+            fields: [
+                {
+                    name: 'Name',
+                    value: socket.guild.name
+                },
+                {
+                    name: 'ID',
+                    value: socket.guild.id
+                },
+                {
+                    name: 'Members',
+                    value: socket.guild.member_count
+                },
+                {
+                    name: 'Owner ID',
+                    value: socket.guild.owner_id
+                },
+                {
+                    name: 'Owner',
+                    value: `${owner.username}#${owner.discriminator}`
+                }
+            ]
+        });
     }
 }
 
--- app/bot/handlers/GuildDeleteEvent.js
+++ app/bot/handlers/GuildDeleteEvent.js
@@ -3,7 +3,37 @@ const EventHandler = require('./EventHandler');
 
 class GuildDeleteEvent extends EventHandler {
     handle(socket) {
-        //
+        app.logger.info(`Left guild with an ID of ${socket.data.id} called: ${socket.data.name}`);
+
+        let avaireCentral = bot.Guilds.find(guild => guild.id === '284083636368834561');
+
+        if (typeof avaireCentral === 'undefined') { // Wat
+            return;
+        }
+
+        let channel = avaireCentral.textChannels.find(channel => channel.name === 'bot-activity');
+        if (typeof channel === 'undefined') {
+            return;
+        }
+
+        return app.envoyer.sendEmbededMessage(channel, {
+            title: 'Left/Was kicked from Server',
+            color: 0xD91616,
+            fields: [
+                {
+                    name: 'Name',
+                    value: socket.data.name
+                },
+                {
+                    name: 'ID',
+                    value: socket.data.id
+                },
+                {
+                    name: 'Owner ID',
+                    value: socket.data.owner_id
+                }
+            ]
+        });
     }
 }
 
