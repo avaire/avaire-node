2955d9394cdb1fb6813953b601172fed05fe6cd9
Alexis Tan <alexis@sen-dev.com>
2017-04-25 20:31:23 +0200

    Create botHas method for permission utils

--- app/utils/permission/Permission.js
+++ app/utils/permission/Permission.js
@@ -8,22 +8,65 @@ class Permission {
      * @return {Boolean}
      */
     requestHas(request, permission) {
-        if (!app.bot.permissions.hasOwnProperty(permission)) {
+        if (!this.isValidPermission(permission)) {
             return false;
         }
 
-        let permissionParts = app.bot.permissions[permission];
-        if (permissionParts.length !== 2) {
-            return false;
-        }
-
-        let group = permissionParts[0];
-        let perms = permissionParts[1];
+        let p = this.preparePermission(permission);
 
         let userGuild = request.message.author.permissionsFor(request.message.guild);
         let userChannel = request.message.author.permissionsFor(request.message.channel);
 
-        return userGuild[group][perms] || userChannel[group][perms];
+        return userGuild[p.group][p.perms] || userChannel[p.group][p.perms];
+    }
+
+    /**
+     * Check if the bot has the given permission.
+     *
+     * @param  {IMessage}  message     The Discordie message object that triggered the command.
+     * @param  {String}    permission  The permission node that should be checked.
+     * @return {Boolean }
+     */
+    botHas(message, permission) {
+        if (!this.isValidPermission(permission)) {
+            return false;
+        }
+
+        let p = this.preparePermission(permission);
+
+        let guild = bot.User.permissionsFor(message.guild);
+        let channel = bot.User.permissionsFor(message.channel);
+
+        return guild[p.group][p.perms] || channel[p.group][p.perms];
+    }
+
+    /**
+     * Checks if the given permission is a valid permission.
+     *
+     * @param  {String}  permission  The permission node that should be checked.
+     * @return {Boolean}
+     */
+    isValidPermission(permission) {
+        if (!app.bot.permissions.hasOwnProperty(permission)) {
+            return false;
+        }
+
+        return app.bot.permissions[permission].length === 2;
+    }
+
+    /**
+     * Prepares the permission to be used.
+     *
+     * @param  {String}  permission  The permission that should be prepared for use.
+     * @return {Object}
+     */
+    preparePermission(permission) {
+        let parts = app.bot.permissions[permission];
+
+        let group = parts[0];
+        let perms = parts[1];
+
+        return {group, perms};
     }
 }
 
