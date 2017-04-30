8ac303deb4e9b6dc606c041957cc73abc6893043
Alexis Tan <alexis@sen-dev.com>
2017-04-30 17:16:28 +0200

    Make the garbage collector delete unused playlists

--- app/bot/jobs/GarbageCollectorJob.js
+++ app/bot/jobs/GarbageCollectorJob.js
@@ -1,5 +1,7 @@
 /** @ignore */
 const Job = require('./Job');
+/** @ignore */
+const Music = require('./../commands/music/MusicHandler');
 
 /**
  * Garbage collector job, this job runs once every minute, every cycle
@@ -24,6 +26,23 @@ class GarbageCollectorJob extends Job {
             if (!cacheAdapter.has(token)) {
                 delete cacheAdapter.cache[token];
             }
+        }
+
+        for (let token in Music.playlist) {
+            let cacheToken = `garbage-collector.music-playlist.${token}`;
+
+            if (Music.playlist[token].length > 0) {
+                continue;
+            }
+
+            if (!cacheAdapter.has(cacheToken)) {
+                cacheAdapter.put(cacheToken, new Date, 120);
+                continue;
+            }
+
+            cacheAdapter.forget(cacheToken);
+
+            Music.forcefullyDeletePlaylist(token);
         }
     }
 }
