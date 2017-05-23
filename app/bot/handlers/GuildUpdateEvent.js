0bfd0f517de619e7a8ebe021a96f0f02460322b4
Alexis Tan <alexis@sen-dev.com>
2017-05-23 21:54:02 +0200

    Update database records on guild update event

--- app/bot/handlers/GuildUpdateEvent.js
+++ app/bot/handlers/GuildUpdateEvent.js
@@ -1,9 +1,25 @@
 /** @ignore */
 const EventHandler = require('./EventHandler');
 
+/**
+ * Emitted when a guild instance the bot is connected to is updated.
+ *
+ * @see http://qeled.github.io/discordie/#/docs/GUILD_UPDATE
+ *
+ * @extends {EventHandler}
+ */
 class GuildUpdateEvent extends EventHandler {
+
+    /**
+     * The event-handler that is executed by Discords event dispatcher.
+     *
+     * @param  {GatewaySocket} socket  The Discordie gateway socket
+     * @return {mixed}
+     */
     handle(socket) {
-        //
+        return app.database.update(app.constants.GUILD_TABLE_NAME, {
+            name: socket.guild.name, owner: socket.guild.owner_id
+        }, query => query.where('id', socket.guild.id));
     }
 }
 
