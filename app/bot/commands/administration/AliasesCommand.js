623164d2f24d5f6d69dca172f9855ae663fc52e2
Alexis Tan <alexis@sen-dev.com>
2017-05-27 01:45:24 +0200

    Add aliases pages

--- app/bot/commands/administration/AliasesCommand.js
+++ app/bot/commands/administration/AliasesCommand.js
@@ -38,16 +38,50 @@ class AliasesCommand extends Command {
      */
     onCommand(sender, message, args) {
         return app.database.getGuild(message.guild.id).then(guild => {
-            let aliasesMessage = [];
             let aliases = guild.get('aliases', {});
-            for (let token in aliases) {
-                aliasesMessage.push(`\`${token}\` => \`${aliases[token]}\``);
+
+            let pageNumber = 1;
+            if (args.length > 0) {
+                pageNumber = parseInt(args[0], 10);
             }
+
+            if (isNaN(pageNumber) || pageNumber < 1) {
+                pageNumber = 1;
+            }
+
+            let pages = Math.ceil(Object.keys(aliases).length / 10);
+            if (pageNumber > pages) {
+                pageNumber = pages;
+            }
+
+            let keys = Object.keys(aliases);
+            let start = 10 * (pageNumber - 1);
+            let aliasesKeys = [];
+            for (let i = start; i < start + 10; i++) {
+                if (keys.length <= i) {
+                    break;
+                }
+
+                let song = keys[i];
+                aliasesKeys.push(keys[i]);
+            }
+
+            let aliasesMessage = [];
+            for (let i in aliasesKeys) {
+                aliasesMessage.push(`\`${aliasesKeys[i]}\` => \`${aliases[aliasesKeys[i]]}\``);
+            }
+
+            let note = [
+                `Page **${pageNumber}** out of **${pages}** pages.`,
+                `\`:command [page number]\``
+            ];
 
             return app.envoyer.sendEmbededMessage(message, {
                 title: 'List of Aliases',
                 color: app.envoyer.colors.success,
-                description: aliasesMessage.join('\n')
+                description: aliasesMessage.join('\n') + '\n\n' + note.join('\n')
+            }, {
+                command: this.getPrefix(message) + this.getTriggers()[0]
             });
         });
     }
